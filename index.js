fb8bcb96ece1b0cfa7d4f62cf6a0e402817ca314
Rastko Strbac <strbac@Macbook-Air-Rastko.local>
2024-08-07 18:41:34 +0200

    Create Gameboard function factory

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 export class Ship {
-   constructor(length) {
-      this.length = length;
+   constructor(size) {
+      this.size = size;
       this.hits = 0;
    }
 
@@ -9,10 +9,52 @@ export class Ship {
    }
 
    isSunk() {
-      if (this.hits >= this.length) {
+      if (this.hits >= this.size) {
          return true;
       } else {
          return false;
       }
    }
 }
+
+export function Gameboard () {
+   // Initialize 10x10 gameboard
+   let gameboardSpaces = [];
+   for (let i = 0; i < 10; i++) {
+      gameboardSpaces[i] = [];
+      for (let j = 0; j < 10; j++) {
+         gameboardSpaces[i][j] = null;
+      }
+   }
+
+   //
+
+   // Place ship at coordinate
+   function placeShip(ship, ...coordinates) {
+      if (ship.size !== coordinates.length) {
+         throw Error("Number of coordinates must match ship length");
+      }
+
+      for (let i = 0; i < ship.size; i++) {
+         let x = coordinates[i][0];
+         let y = coordinates[i][1];
+         gameboardSpaces[x][y] = ship;
+      }
+   }
+
+   // Recieve attack function
+   function receiveAttack(coordinates) {
+      let x = coordinates[0];
+      let y = coordinates[1];
+      if (gameboardSpaces[x][y] !== null) {
+         let shipHit = gameboardSpaces[x][y];
+         shipHit.hit();
+         return `Ship hit! Ship health: ${shipHit.size - shipHit.hits}`;
+      } else {
+         gameboardSpaces[x][y] = "miss";
+         return "Missed!";
+      }
+   }
+
+   return { gameboardSpaces, placeShip, receiveAttack };
+};
--- index.test.js
+++ index.test.js
@@ -1,4 +1,5 @@
 import { Ship } from "./index.js";
+import { Gameboard } from "./index.js";
 
 describe("Ship isSunk tests", () => {
    test("ship is sunk after one hit : false", () => {
@@ -18,6 +19,44 @@ describe("Ship isSunk tests", () => {
    });
 });
 
-describe("Gameboard test methods", () => {
-   
+describe("Gameboard ship placement tests", () => {
+   let gameboard;
+
+   beforeEach(() => {
+      gameboard = Gameboard();
+   });
+
+   test("ship placement", () => {
+      const ship = new Ship(3);
+      gameboard.placeShip(ship, [2, 4], [2, 5], [2, 6]);
+      expect(gameboard.gameboardSpaces[2][4]).toEqual(ship);
+   });
+
+   test("ship placement throw error", () => {
+      const ship = new Ship(3);
+      expect(() => {
+         gameboard.placeShip(ship, [2, 4], [2, 5], [2, 6], [2, 7]);
+      }).toThrow("Number of coordinates must match ship length");
+   });
+});
+
+describe("Gameboard receiveAttack tests", () => {
+   let gameboard;
+
+   beforeEach(() => {
+      gameboard = Gameboard();
+   });
+
+   test("ship hits after attack", () => {
+      const ship = new Ship(5);
+      gameboard.placeShip(ship, [2, 4], [2, 5], [2, 6], [2, 7], [2, 8]);
+      gameboard.receiveAttack([2, 4]);
+      expect(ship.hits).toBe(1);
+   });
+
+   test("shot missed", () => {
+      const ship = new Ship(5);
+      gameboard.placeShip(ship, [2, 4], [2, 5], [2, 6], [2, 7], [2, 8]);
+      expect(gameboard.receiveAttack([7, 7])).toBe("Missed!");
+   });
 });
